2c51e3643d4a9b6c6a40408e038a2b3a29294d9b
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-01-20 12:16:40 -0600

    even or odd and example, complete

---
+++ src/tests/even_or_odd.test.js
@@ -0,0 +1,11 @@
+const evenOrOdd = require('../challenges/even_or_odd.js');
+
+
+test('checks to see if  18 is even', () => {
+    expect(evenOrOdd(18)).toBe("Even");
+  });
+
+
+test('checks to see if  11 is odd', () => {
+    expect(evenOrOdd(11)).toBe("Odd");
+  });
