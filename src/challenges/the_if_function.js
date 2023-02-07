045d54a84e67807af80ca8b100414c5c01072489
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-07 09:07:28 -0600

    the if function

--- src/challenges/the_if_function.js
+++ src/challenges/the_if_function.js
@@ -1,9 +1,9 @@
 function _if(bool, func1, func2) {
-    if (bool) {
-        func1()
-    } else {
-        func2()
-    }
+  if (bool) {
+    func1()
+  } else {
+    func2()
   }
+}
 
-module.exports = _if; 
+module.exports = _if
--- src/tests/the_if_function.test.js
+++ src/tests/the_if_function.test.js
@@ -1,5 +1,5 @@
-const _if = require('../challenges/the_if_function.js');
+const _if = require("../challenges/the_if_function.js")
 
 test("true should call func1", () => {
-  expect(_if(true, func1, func2)).toBe(func1());
-}); 
+  expect(_if(true, func1, func2)).toBe(func1())
+})
