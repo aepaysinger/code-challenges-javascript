75744914e200416ca717eb21d8ec70139bc1c4c9
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-08 08:47:15 -0600

    the if function, complete

--- src/challenges/the_if_function.js
+++ src/challenges/the_if_function.js
@@ -6,12 +6,4 @@ function _if(bool, func1, func2) {
   }
 }
 
-function func1() {
-  return "function 1 was called"
-}
-
-function func2() {
-  return "function 2 was called"
-}
-
-;(module.exports = _if), func1, func2
+module.exports = _if
--- src/tests/the_if_function.test.js
+++ src/tests/the_if_function.test.js
@@ -1,6 +1,12 @@
 const _if = require("../challenges/the_if_function.js")
-const func1 = require("../challenges/the_if_function.js")
-const func2 = require("../challenges/the_if_function.js")
+
+function func1() {
+  return "function 1 was called"
+}
+
+function func2() {
+  return "function 2 was called"
+}
 
 test("true should call func1", () => {
   expect(_if(true, func1, func2)).toBe("function 1 was called")
