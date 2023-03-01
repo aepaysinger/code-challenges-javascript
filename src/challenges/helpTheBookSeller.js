e0371d5558b30a6697223169cc1bbf4af5c703e8
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-01 13:41:47 -0600

    help the bookseller complete

--- src/challenges/helpTheBookSeller.js
+++ src/challenges/helpTheBookSeller.js
@@ -1,14 +1,27 @@
 const stockList = (listOfArt, listOfCat) => {
   let codeTotals = {}
-  listOfArt.split(" ").forEach((element) => {
-    console.log(element)
-  })
-}
+  let finalResult = ""
 
-console.log(
-  stockList(
-    ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"],
-    ["A", "B", "C", "D"]
-  )
-)
-//"(A : 0) - (B : 1290) - (C : 515) - (D : 600)"
+  if (listOfArt.length === 0 || listOfCat.length === 0) {
+    return ""
+  }
+  listOfArt.forEach((book) => {
+    console.log(book.split(" ")[1])
+    if (book.split(" ")[0][0] in codeTotals) {
+      codeTotals[book.split(" ")[0][0]] += parseInt(book.split(" ")[1])
+    } else {
+      codeTotals[book.split(" ")[0][0]] = parseInt(book.split(" ")[1])
+    }
+  })
+  listOfCat.forEach((letter) => {
+    if (!(letter in codeTotals)) {
+      codeTotals[letter] = 0
+    }
+  })
+  listOfCat.forEach((letter) => {
+    finalResult += `(${letter} : ${codeTotals[letter]}) - `
+  })
+  return finalResult.slice(0, -3)
+}
+console.log(stockList([], ["A", "B", "C", "D"]))
+module.exports = stockList
