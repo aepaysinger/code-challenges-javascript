cd25c2093e195e44a6636cbf63c37effa55c4f0e
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-05-02 09:06:03 -0500

    read-me

---
+++ src/challenges/practice.js
@@ -0,0 +1,6 @@
+function incrementItems(arr) {
+  for (let i = 0; i < array.length; i++) arr[i] === arr[i] + 1
+  return array
+}
+
+incrementItems([0, 1, 2, 3])
--- src/challenges/price_of_magoes.js
+++
@@ -1,7 +0,0 @@
-function mango(quantity, price) {
-  dealPrice = price * 2
-  totalPrice = Math.floor(quantity / 3) * dealPrice
-  totalPrice += (quantity % 3) * price
-  return totalPrice
-}
-module.exports = mango
