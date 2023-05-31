0dde444a2e9a2df8a9fb1f0ef18c5df8926bb49b
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-05-31 09:20:39 -0500

    string incrementer

--- src/challenges/stringIncrementer.js
+++ src/challenges/stringIncrementer.js
@@ -1,19 +1,44 @@
 const incrementString = (strng) => {
+  if (isNaN(strng.slice(-1))) {
+    return strng + "1"
+  }
+
+  strng = strng.split("")
+  strng = strng.reverse()
+
   let strngNumber = ""
-  for (let character of strng) {
+
+  strng.forEach((character) => {
     if (!isNaN(character)) {
-      strngNumber += character
+      strngNumber = character + strngNumber
+    }
+  })
+
+  for (let i = 0; i < strngNumber.length; i++) {
+    if (strngNumber[i] === "0") {
+    } else {
+      console.log(strngNumber.slice(i, -1))
+      let notZero = parseInt(strngNumber.slice(i, -1))
+      notZero += 1
+      notZero = notZero.toString()
+      strng = strng.reverse()
+      strng = strng.join("")
+      stop = strngNumber.length
+      return strng.slice(0, -(stop - 1)) + strngNumber.slice(0, i) + notZero
     }
   }
-  //   console.log(strngNumber)
-  let numberToIncrease = parseInt(strngNumber)
-  //   console.log(numberToIncrease)
-  if (numberToIncrease === " ") {
-    return strng + "1"
-  } else {
-    numberToIncrease += 1
-    return strng + numberToIncrease.toString()
-  }
-}
 
-console.log(incrementString("foo1"))
+  strngNumber = parseInt(strngNumber)
+  strngNumber += 1
+
+  strng = strng.reverse()
+  strng = strng.join("")
+  strngNumber = strngNumber.toString()
+  stop = strngNumber.length
+
+  return strng.slice(0, -(stop - 1)) + strngNumber
+}
+console.log(incrementString("foobar001"))
+// step one: stringvariable.split("") = ['s', 't', 'r', 'i', 'n', 'g']
+// step two: stringvariable.reverse() = ['g', 'n', 'i', 'r', 't', 's']
+//step three: stringvariable.join("") = "gnirts"
