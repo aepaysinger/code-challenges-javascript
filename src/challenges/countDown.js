363a6a412ec3518a3650e727e4745340b4d1bade
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-08-28 10:59:13 -0500

    count down

--- src/challenges/countDown.js
+++ src/challenges/countDown.js
@@ -1,11 +1,19 @@
 const countDown = (n) => {
+  // Base case
   if (n < 1) {
     return []
   } else {
     const countArray = countDown(n - 1)
-    countArray.unshift(n)
+    countArray.push(3 - n + 1)
     return countArray
   }
 }
+console.log(countDown(3))
+// module.exports = countDown
+// countDown(1)
+// countDown(2)
+// countDown(3)[(3, 2, 1)]
 
-module.exports = countDown
+// 3
+// 2
+// 1
